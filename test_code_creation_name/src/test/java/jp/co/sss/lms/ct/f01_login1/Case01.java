5e6503aa63d9e821faf0365833fc6df85967b3ff
hj2512 <hjidapa2512@gmail.com>
2026-09-18 15:45:40 +0900

    add:Case01・Case02.java
    Case01とCase02の結合テストコードを追加しました。
    
    【Case01】
    ログイン画面への遷移を確認するテストを作成しました。
    
    期待値は以下の通りです。
    ・タイトル：ログイン | LMS
    ・ログインボタン：ログイン
    
    【Case02】
    DBに登録されていないユーザーでログインし、
    エラーメッセージが期待値通りに表示されるかを確認するテストを作成しました。
    
    期待値は以下の通りです。
    ・エラーメッセージ：* ログインに失敗しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,7 +37,24 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// トップページにアクセス
+		goTo("http://localhost:8080/lms/");
+
+		// タイトルを取得
+		String title = webDriver.getTitle();
+
+		// ログインボタンの文字を取得
+		String loginButton = webDriver.findElement(
+				By.cssSelector("input[type='submit']"))
+				.getAttribute("value");
+
+		// 期待値と比較する
+		assertEquals("ログイン | LMS", title);
+		assertEquals("ログイン", loginButton);
+
+		// 画面を保存する
+		getEvidence(new Object() {
+		}, "01_トップページ");
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,42 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		goTo("http://localhost:8080/lms/");
+
+		webDriver.findElement(By.id("loginId"))
+				.sendKeys("StudentAB99");
+
+		webDriver.findElement(By.id("password"))
+				.sendKeys("Test1234");
+
+		webDriver.findElement(By.cssSelector("input[type='submit']"))
+				.click();
+
+		//エラーメッセージが表示されるまで待つ
+		visibilityTimeout(
+				By.cssSelector("span.help-inline.error"), 5);
+
+		//画面に表示された文字を取得する
+		String errorMessage = webDriver.findElement(
+				By.cssSelector("span.help-inline.error"))
+				.getText();
+
+		System.out.println(errorMessage);
+
+		//期待結果と実際の結果の確認
+		assertEquals("* ログインに失敗しました。", errorMessage);
+
+		//テスト結果の画面保存
+		getEvidence(new Object() {
+		}, "02_認証失敗");
 	}
 
 }
